93f7eb16c7a1120d8288de165035be16969e9b55
Javier Exeni <jvexeni@gmail.com>
2025-09-22 16:12:43 -0400

    feat(landing): create navbar component for the landing layout

---
+++ src/app/community-front/components/dt-navbar/dt-navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dt-navbar',
+  imports: [],
+  templateUrl: './dt-navbar.html',
+  styles: ``
+})
+export class DtNavbar {
+
+}
--- src/app/community-front/layouts/community-layout/community-layout.ts
+++ src/app/community-front/layouts/community-layout/community-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DtNavbar } from "../../components/dt-navbar/dt-navbar";
 
 @Component({
   selector: 'app-community-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DtNavbar],
   templateUrl: './community-layout.html',
   styles: ``
 })
