0b2b671786957226019ab245364a1a899b9a3a95
Javier Exeni <jvexeni@gmail.com>
2025-09-22 20:16:14 -0400

    feat(landing): add footer component

---
+++ src/app/community-front/components/dt-footer/dt-footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dt-footer',
+  imports: [],
+  templateUrl: './dt-footer.html',
+  styles: ``
+})
+export class DtFooter {
+
+}
